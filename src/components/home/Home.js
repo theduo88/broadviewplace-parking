16aeb587cb477aa9be9d5bf0b90675b6941619e0
Troy Zhang <troy_d_zhang@homedepot.com>
2019-01-23 15:29:44 -0500

    adding main screen

---
+++ src/components/home/Home.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+class Home extends Component {
+
+    render()    {
+        return  (
+            <React.Fragment>
+                <Paper elevation={10}>
+                    <Grid container spacing={24}>
+                        <Grid item xs={12} sm={3}>
+                            <TextField
+                                required
+                                id="licenPlateNumber"
+                                name="licenPlateNumber"
+                                label="License Plate Number"
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={3}>
+                            <Button variant="contained" color="primary">
+                                Check Plate
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </React.Fragment>
+        )
+    }
+
+}
+export default Home;
